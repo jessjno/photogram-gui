332c564e043b39afc275c40d87b118a306593f98
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-10-02 00:16:28 +0000

    add photo create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,46 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/index" })
   end
+
+  def show
+    # Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+
+
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show" })
+  end
+
+  def delete
+    #  Parameters: {"path_id"=>"765"}
+
+    the_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    redirect_to("/photos/" + a_new_photo.id.to_s )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "delete" })
+  get("/insert_photo", { :controller => "photos", :action => "create" })
 
 end
